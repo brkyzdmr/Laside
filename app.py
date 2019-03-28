c6a1b8b3a6fe4555efda7b922ab2b0a395e4fb67
Berkay Ezdemir <berkayezdemir@hotmail.com>
2019-03-29 02:10:31 +0300

    dict screen added

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 from main_screen import  Ui_MainScreen
 from save_word_screen import Ui_SaveWordScreen
-
+from dictionary_screen import  Ui_DictionaryScreen
 class MainScreen(QMainWindow):
     def __init__(self, parent=None):
         super(MainScreen, self).__init__(parent=parent)
@@ -13,8 +13,11 @@ class MainScreen(QMainWindow):
         window = SaveWordScreen(self)
         window.show()
         self.hide()
-    def btn_lookup_clicked(self):
-        print("Look Up Clicked!")
+    def btn_dictionary_clicked(self):
+        print(" Clicked!")
+        window = DictionaryScreen(self)
+        window.show()
+        self.hide()
     def btn_settings_clicked(self):
         print("Settings clicked!")
 
@@ -30,6 +33,15 @@ class SaveWordScreen(QMainWindow):
     def btn_save_clicked(self):
         print("Save clicked!")
 
+class DictionaryScreen(QMainWindow):
+    def __init__(self, parent=None):
+        super(DictionaryScreen, self).__init__(parent=parent)
+        self.ui = Ui_DictionaryScreen()
+        self.ui.setupUi(self)
+    def btn_back_clicked(self):
+        window = MainScreen(self)
+        window.show()
+        self.hide()
 
 def main():
     app = QApplication(sys.argv)
