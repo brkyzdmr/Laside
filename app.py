5846456ca5fda6dfc6522e0f3c4398a573e2d68d
Berkay Ezdemir <berkayezdemir@hotmail.com>
2019-03-29 07:02:24 +0300

    firebase db added

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-import sys
+import sys, sqlite3, re, string
 from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
 from main_screen import  Ui_MainScreen
 from save_word_screen import Ui_SaveWordScreen
 from dictionary_screen import  Ui_DictionaryScreen
+from firebase import firebase
+
+
 class MainScreen(QMainWindow):
     def __init__(self, parent=None):
         super(MainScreen, self).__init__(parent=parent)
@@ -32,6 +35,26 @@ class SaveWordScreen(QMainWindow):
         self.hide()
     def btn_save_clicked(self):
         print("Save clicked!")
+        word = self.ui.tb_word.text()
+        meaning = self.ui.tb_meaning.toPlainText()
+
+        test_word = re.sub('[^A-Za-z]+', '', word)
+        test_meaning = re.sub('[^A-Za-z]+', '', meaning)
+        if test_word != "" and test_meaning != "":
+            print(test_word + ", " + test_meaning)
+            word.lower()
+            meaning.lower()
+            meaning_words = meaning.split(",")
+
+            data = {
+                "word": word,
+                "meaning": meaning_words
+            }
+
+            
+            fb_con = firebase.FirebaseApplication("https://laside-6b158.firebaseio.com/", None)
+            result = fb_con.post("/users/user2", data)
+            print(result)      
 
 class DictionaryScreen(QMainWindow):
     def __init__(self, parent=None):
@@ -46,7 +69,6 @@ class DictionaryScreen(QMainWindow):
 def main():
     app = QApplication(sys.argv)
     window = MainScreen()
-
 
     window.show()
     sys.exit(app.exec_())
